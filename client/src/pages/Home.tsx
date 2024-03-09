b0bb35015be3a0d75d637686b9921048cc49d91f
daljeet723 <dkaur5842@gmail.com>
2024-03-09 18:45:01 +0530

    remoce default files

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className='home-container'>
+      <p>Check the github repo :</p>
+      <a href='https://github.com/daljeet723/MERN-stack-authentication/blob/master'>
+        MERN stack authentication guide
+      </a>
+    </div>
+  )
+}
+
+export default Home
